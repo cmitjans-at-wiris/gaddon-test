583a44f0a57344a2494f6898e89ab28c0d3beae4
Carles Mitjans <cmitjans@wiris.com>
2021-12-01 16:44:57 +0100

    chore: Rename simple_triggers file

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { onOpen, openTaskpane } from '@src/hello';
+import { onOpen, openTaskpane } from '@src/simple_triggers';
 
 (global as any).onOpen = onOpen;
 (global as any).openTaskpane = openTaskpane;
--- src/simple_triggers.ts
+++ src/simple_triggers.ts
@@ -1,20 +1,15 @@
 /* global DocumentApp, HtmlService */
 
-const onOpen = () => {
+export const onOpen = () => {
   const addonMenu = DocumentApp.getUi().createAddonMenu();
   addonMenu.addItem('Open sidebar', 'openTaskpane');
   addonMenu.addToUi();
 };
 
-const openTaskpane = () => {
+export const openTaskpane = () => {
   const htmlServer = HtmlService.createTemplateFromFile('taskpane-html.html');
   const html = htmlServer.evaluate();
 
   const ui = DocumentApp.getUi();
   ui.showSidebar(html);
-};
-
-export {
-  onOpen,
-  openTaskpane,
 };
